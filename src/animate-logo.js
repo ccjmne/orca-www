69c6b0362d68a2a77f552a35b7759965d378bea2
ccjmne <ccjmne@gmail.com>
2019-01-27 00:59:45 +0100

    Add shine effect to logo animation

--- src/animate-logo.js
+++ src/animate-logo.js
@@ -5,7 +5,9 @@ const { 'default': anime } = require('animejs');
 const svgns = 'http://www.w3.org/2000/svg',
   selector = '.hero svg.animated-logo',
   drawDuration = 2000,
-  easing = 'easeInOutQuad';
+  easing = 'easeInOutQuad',
+  svg = document.querySelector(selector),
+  shine = svg.querySelector('rect.shine');
 
 const strokes = [].slice.call(document.querySelectorAll(`${ selector } path[stroke][delay]`)).map(s => {
   const c = document.createElementNS(svgns, 'circle');
@@ -17,18 +19,24 @@ const strokes = [].slice.call(document.querySelectorAll(`${ selector } path[stro
   return c;
 });
 
+function doShine() {
+  anime({ targets: shine, x: [-125, 540], duration: 800, easing });
+}
+
 setTimeout(() => {
-  document.querySelector(selector).style.visibility = 'visible';
-  anime({ targets: strokes, r: e => [0, e.getAttribute('radius')], duration: 500, easing: easing, delay: e => e.getAttribute('delay') })
-    .finished.then(() => anime({ targets: strokes, r: e => [e.getAttribute('radius'), 0], easing: easing, duration: 500, delay: 300 }));
+  svg.style.visibility = 'visible';
+  svg.addEventListener('mouseenter', doShine);
+  svg.addEventListener('touchstart', doShine);
+  anime({ targets: strokes, r: e => [0, e.getAttribute('radius')], duration: 500, easing, delay: e => e.getAttribute('delay') })
+    .finished.then(() => anime({ targets: strokes, r: e => [e.getAttribute('radius'), 0], easing, duration: 500, delay: 300 }));
   anime({
     targets: strokes,
     translateX: x => x.pathExtractor('x'),
     translateY: x => x.pathExtractor('y'),
     duration: e => drawDuration - parseInt(e.getAttribute('delay')) + 1000,
-    easing: easing,
+    easing,
     delay: e => e.getAttribute('delay')
-  }).finished.then(() => anime({ targets: `${ selector } path[stroke]`, 'stroke-opacity': '0', duration: 1000, easing: easing }));
-  anime({ targets: `${ selector } path[fill][fill-opacity="0"]`, 'fill-opacity': [0, 1], easing: easing, duration: 2000, delay: 2000 });
-  anime({ targets: `${ selector } path[stroke]`, strokeDashoffset: [anime.setDashoffset, 0], duration: e => drawDuration - parseInt(e.getAttribute('delay')) + 1000, easing: easing, delay: e => e.getAttribute('delay') });
+  }).finished.then(() => anime({ targets: `${ selector } path[stroke]`, 'stroke-opacity': '0', duration: 1000, easing })).then(doShine);
+  anime({ targets: `${ selector } path[fill][fill-opacity='0']`, 'fill-opacity': [0, 1], easing, duration: 2000, delay: 2000 });
+  anime({ targets: `${ selector } path[stroke]`, strokeDashoffset: [anime.setDashoffset, 0], duration: e => drawDuration - parseInt(e.getAttribute('delay')) + 1000, easing, delay: e => e.getAttribute('delay') });
 }, 0);
