0e07a27545bcb55837a6dcecebabf58483f7e912
ccjmne <ccjmne@gmail.com>
2019-01-28 23:12:16 +0100

    Replace skeleton-css with skeleton-scss

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-require('./node_modules/skeleton-css/css/normalize.css');
-require('./node_modules/skeleton-css/css/skeleton.css');
 require('./main.scss');
 
 require('./src/animate-logo');
