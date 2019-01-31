2e66d87b825c4357e94e9c2e978a20d0e86fe683
ccjmne <ccjmne@gmail.com>
2019-01-31 05:58:36 +0100

    Major refactor of interactive-chart

--- src/interactive-chart/interactive-chart.js
+++ src/interactive-chart/interactive-chart.js
@@ -1,7 +1,5 @@
 'use strict';
-/* jshint camelcase: false */
-
-const $scope = { overTime: {} };
+// jshint camelcase: false
 
 import { bisector, extent, max } from 'd3-array';
 import { axisLeft, axisBottom } from 'd3-axis';
@@ -13,203 +11,185 @@ import { area, line, curveMonotoneX as curve } from 'd3-shape';
 import { timeFormat } from 'd3-time-format';
 import 'd3-transition';
 
+const certificates = Object.values(require('./certificates.sample.json')).sort(({ cert_order }) => cert_order);
+const data = require('./stats.sample.json');
 const dateBisector = bisector(({ date }) => date).left;
 
-function getClosestEntry(data, scale, pos) {
-  const date = scale.invert(pos),
-    idx = dateBisector(data, date),
-    lo = data[idx - 1] || {},
-    hi = data[idx] || {};
-  return date - lo.date > hi.date - date ? hi : lo;
-}
+class InteractiveChart {
+  constructor(selector) {
+    this.svg = select(selector).attr('viewBox', `0 0 ${ this.width } ${ this.height }`).attr('preserveAspectRatio', 'xMidYMid meet')
+      .append('g').attr('transform', `translate(${ this.margin.left }, ${ this.margin.top })`);
 
-const transition = (element, duration = 50) => element.transition().duration(duration).ease(easing);
+    this.axes = {
+      x: {
+        grid: this.svg.append('g').attr('class', 'x grid').attr('transform', `translate(0, ${ this.chartHeight })`),
+        elem: this.svg.append('g').attr('class', 'x axis').attr('transform', `translate(0, ${ this.chartHeight })`),
+        scale: scaleTime().range([0, this.chartWidth]).nice(),
+        axis: axisBottom().ticks(this.chartWidth / 100)
+      },
+      y: {
+        grid: this.svg.append('g').attr('class', 'y grid'),
+        elem: this.svg.append('g').attr('class', 'y axis'),
+        scale: scaleLinear().range([this.chartHeight, 0]).nice(),
+        axis: axisLeft().ticks(this.chartHeight / 30)
+      }
+    };
 
-let xAxis, yAxis, areaAboveCount, areaAboveTarget, areaBelowCount, areaBelowTarget, certLegend, x, y, svg, targetLine, countLine;
-let certData;
+    const defs = this.svg.append('defs');
+    ['count', 'target'].forEach(value => this[value] = (({ x, y }) => ({
+      line: {
+        elem: this.svg.append('path').attr('class', `${ value } line`),
+        fn: line().curve(curve).x(x).y(y)
+      },
+      area: {
+        elem: this.svg.append('path').attr('class', `${ value } area`).attr('clip-path', `url(#clip-${ value })`),
+        clip: defs.append('clipPath').attr('id', `clip-${ value }`).append('path'),
+        above: area().curve(curve).x(x).y0(y).y1(0),
+        below: area().curve(curve).x(x).y0(y).y1(this.chartHeight)
+      }
+    }))({ x: ({ date }) => this.axes.x.scale(date), y: d => this.axes.y.scale(d[value]) }));
 
-// ---------------------------------------------------------
-// SVG CREATION
-// ---------------------------------------------------------
+    this.crosshairs = (g => ({
+      g,
+      ticks: g.select('#abscissa .axis'),
+      date: g.select('#abscissa'),
+      count: g.select('#ordinate-count'),
+      target: g.select('#ordinate-target')
+    }))(this.svg.append('g').style('opacity', 0).html(`
+      <g id="abscissa">
+          <path class="highlight-coordinates" d="M0,0V${ this.chartHeight }"></path>
+          <g class="axis">
+              <path d="M0,${ this.chartHeight }v25"></path>
+              <text transform="translate(0, ${ this.chartHeight + 37 })" text-anchor="middle"></text>
+          </g>
+      </g>
+      <path id="ordinate-count" class="highlight-coordinates" d="M0,0H${ this.chartWidth }"></path>
+      <path id="ordinate-target" class="highlight-coordinates" d="M0,0H${ this.chartWidth }"></path>`));
 
-function initSvg(placeholder) {
-  const margin = { top: 20, right: 80, bottom: 35, left: 30 };
-  const w = 400,
-    h = 180;
+    this.details = (g => ({
+      g,
+      count: g.select('#highlight-count'),
+      target: g.select('#highlight-target')
+    }))(this.svg.append('g').style('opacity', 0).html(['count', 'target'].map(type => `
+      <g id="highlight-${ type }" text-anchor="middle">
+          <circle class="line ${ type }" r="4"></circle>
+          <text class="highlight-shadow"></text>
+          <text class="${ type }"></text>
+      </g>`).reverse()));
 
-  const width = w - margin.left - margin.right,
-    height = h - margin.top - margin.bottom;
+    this.legend = this.svg.append('g').attr('transform', `translate(${ this.chartWidth }, ${ this.chartHeight / 2 })`).html(`
+      <path class="line count" d="M10,-10h25"></path>
+      <path class="line target" d="M10,10h25"></path>
+      <text id="legend-cert" class="count" transform="translate(40, -5)"></text>
+      <text class="target" transform="translate(40, 15)">Cible</text>`).select('#legend-cert');
 
-  placeholder
-    .attr('viewBox', `0 0 ${ w } ${ h }`)
-    .attr('preserveAspectRatio', 'xMidYMid meet');
-
-  svg = placeholder.append('g').attr('transform', `translate(${ margin.left }, ${ margin.top })`);
-
-  x = scaleTime().range([0, width]).nice();
-  y = scaleLinear().range([height, 0]).nice();
-
-  xAxis = axisBottom().ticks(width / 100);
-  yAxis = axisLeft().ticks(height / 30);
-
-  // GRID
-  const grid = svg.append('g');
-  grid.append('g').attr('class', 'grid x').attr('transform', `translate(0, ${ height })`);
-  grid.append('g').attr('class', 'grid y');
-
-  // AXES
-  svg.append('g').attr('transform', `translate(0, ${ height })`).attr('class', 'x axis');
-  svg.append('g').attr('class', 'y axis');
-
-  // ---------------------------------------------------------
-  // LINES AND AREAS FUNCTIONS DEFINITION
-  // ---------------------------------------------------------
-
-  countLine = line().curve(curve).x(d => x(d.date)).y(d => y(d.count));
-  targetLine = line().curve(curve).x(countLine.x()).y(d => y(d.target));
-
-  areaAboveCount = area().curve(curve).x(countLine.x()).y0(countLine.y()).y1(0);
-  areaAboveTarget = area().curve(curve).x(targetLine.x()).y0(targetLine.y()).y1(0);
-  areaBelowCount = area().curve(curve).x(countLine.x()).y0(countLine.y()).y1(height);
-  areaBelowTarget = area().curve(curve).x(targetLine.x()).y0(targetLine.y()).y1(height);
-
-  // ---------------------------------------------------------
-  // SVG ELEMENTS PLACEHOLDERS
-  // ---------------------------------------------------------
-
-  // CLIPPING AREAS
-  const defs = svg.append('defs');
-  defs.append('clipPath').attr('id', 'clip-count').append('path');
-  defs.append('clipPath').attr('id', 'clip-target').append('path');
-
-  // AREAS
-  svg.append('path').attr('class', 'count area').attr('clip-path', 'url(#clip-target)');
-  svg.append('path').attr('class', 'target area').attr('clip-path', 'url(#clip-count)');
-
-  // COORDINATES HIGHLIGHT PLACEHOLDER
-  const highlightCoordinates = svg.append('g').style('display', 'none');
-
-  // LINES
-  svg.append('path').attr('class', 'target line');
-  svg.append('path').attr('class', 'count line');
-
-  // ---------------------------------------------------------
-  // HIGHLIGHT ELEMENTS
-  // ---------------------------------------------------------
-
-  const highlight = svg.append('g').style('display', 'none');
-
-  const highlightAbscissa = highlightCoordinates.append('g');
-  highlightAbscissa.append('path').attr('d', `M0,0V${ height }`).attr('class', 'highlight-coordinates');
-  const highlightAbscissaTick = highlightAbscissa.append('g').attr('class', 'axis');
-  highlightAbscissaTick.append('path').attr('d', `M0,${ height }v20`);
-  highlightAbscissaTick.append('text').attr('transform', `translate(0, ${ height + 30 })`).attr('text-anchor', 'middle');
-
-  const highlightOrdinateCount = highlightCoordinates.append('path').attr('d', `M0,0H${ width }`).attr('class', 'highlight-coordinates');
-  const highlightOrdinateTarget = highlightCoordinates.append('path').attr('d', `M0,0H${ width }`).attr('class', 'highlight-coordinates');
-
-  const highlightTarget = highlight.append('g').attr('text-anchor', 'middle');
-  highlightTarget.append('circle').attr('class', 'target line').attr('r', 4);
-  highlightTarget.append('text').attr('class', 'highlight-shadow');
-  highlightTarget.append('text').attr('class', 'target');
-
-  const highlightCount = highlight.append('g').attr('text-anchor', 'middle');
-  highlightCount.append('circle').attr('class', 'count line').attr('r', 4);
-  highlightCount.append('text').attr('class', 'highlight-shadow');
-  highlightCount.append('text').attr('class', 'count');
-
-  const legend = svg.append('g').attr('transform', `translate(${ width }, ${ height / 2 })`).style('fill', 'black');
-  legend.append('path').attr('d', 'M10,-10h25').attr('class', 'line count');
-  //legend.append('circle').attr('r', 4).attr('cx', 30).attr('cy', -10).attr('class', 'line count');
-  certLegend = legend.append('text').attr('class', 'count').attr('transform', 'translate(40, -5)');
-  legend.append('path').attr('d', 'M10,10h25').attr('class', 'line target').style('shape-rendering', 'crispEdges');
-  //legend.append('circle').attr('r', 4).attr('cx', 30).attr('cy', 10).attr('class', 'line target');
-  legend.append('text').attr('class', 'target').attr('transform', 'translate(40, 15)').text('Cible');
-
-  // ---------------------------------------------------------
-  // HOVERING ZONE
-  // ---------------------------------------------------------
-
-  svg.append('rect').attr('width', width).attr('height', height).style('fill', 'none').style('pointer-events', 'all').style('cursor', 'crosshair')
-    .on('mouseover', function () {
-      highlight.style('display', null);
-      highlightCoordinates.style('display', null);
-      grid.style('display', 'none');
-    })
-    .on('mouseout', function () {
-      highlight.style('display', 'none');
-      highlightCoordinates.style('display', 'none');
-      grid.style('display', null);
-      $scope.mouseY = null;
-    })
-    .on('mousemove', function () {
-      $scope.mouseY = mouse(this)[0];
-      $scope.updateHighlight();
+    const self = this;
+    this.svg.append('rect').attr('class', 'hover-zone').attr('width', this.chartWidth).attr('height', this.chartHeight).on('mouseover', function () {
+      self.details.g.transition().style('opacity', '1');
+      self.crosshairs.g.transition().style('opacity', '1');
+    }).on('mouseout', function () {
+      self.details.g.transition().style('opacity', 0);
+      self.crosshairs.g.transition().style('opacity', 0);
+      self.mouseX = null;
+    }).on('mousemove', function () {
+      [self.mouseX, ] = mouse(this);
+      self.updateHighlight();
     });
+  }
 
-  $scope.updateHighlight = function (transitionDuration = 50) {
-    if ($scope.mouseY) {
-      const d = getClosestEntry(certData, x, $scope.mouseY);
-      highlightCount.transition().duration(transitionDuration).ease(easing).attr('transform', `translate(${ x(d.date) }, ${ y(d.count) })`);
-      highlightCount.selectAll('text')
-        .transition().duration(transitionDuration).ease(easing)
-        .attr('transform', 'translate(0, ' + (d.target > d.count ? 17 : -8) + ')')
-        .text($scope.overTime.cert.cert_short + ' : ' + d.count);
+  updateHighlight(duration = 50) {
+    const transition = this.transition(duration);
+    const { date, count, target } = this.getClosestEntry();
+    const { x: { scale: scaleX }, y: { scale: scaleY } } = this.axes;
 
-      highlightTarget.transition().duration(transitionDuration).ease(easing).attr('transform', `translate(${ x(d.date) }, ${ y(d.target) })`);
-      highlightTarget.selectAll('text')
-        .transition().duration(transitionDuration).ease(easing)
-        .attr('transform', 'translate(0, ' + (d.target > d.count ? -8 : 17) + ')')
-        .text('Cible : ' + d.target);
+    transition(this.details.count).attr('transform', `translate(${ scaleX(date) }, ${ scaleY(count) })`);
+    transition(this.details.count.selectAll('text'))
+      .attr('transform', `translate(0, ${ target > count ? 17 : -8 })`)
+      .text(`${ this.cert.cert_short } : ${ count }`);
 
-      highlightAbscissa.transition().duration(transitionDuration).ease(easing).attr('transform', `translate(${ x(d.date) }, 0)`);
-      highlightOrdinateCount.transition().duration(transitionDuration).ease(easing).attr('transform', `translate(0, ${ y(d.count) })`);
-      highlightOrdinateTarget.transition().duration(transitionDuration).ease(easing).attr('transform', `translate(0, ${ y(d.target) })`);
+    transition(this.details.target).attr('transform', `translate(${ scaleX(date) }, ${ scaleY(target) })`);
+    transition(this.details.target.selectAll('text'))
+      .attr('transform', `translate(0, ${ target > count ? -8 : 17 })`)
+      .text(`Cible : ${ target }`);
 
-      highlightAbscissaTick.select('text').text(timeFormat('%d %b %Y')(d.date));
+    transition(this.crosshairs.date).attr('transform', `translate(${ scaleX(date) }, 0)`);
+    transition(this.crosshairs.count).attr('transform', `translate(0, ${ scaleY(count) })`);
+    transition(this.crosshairs.target).attr('transform', `translate(0, ${ scaleY(target) })`);
+    transition(this.crosshairs.ticks).select('text').text(timeFormat('%d %b %Y')(date));
+  }
+
+  displayData({ cert_pk: key }, duration = 1000) {
+    this.cert = certificates.find(c => c.cert_pk === key);
+    this.data = data.map(({ date, stats }) => ({ date: new Date(date), count: stats[key].current, target: stats[key].target }));
+    this.legend.transition().duration(duration / 2).style('opacity', 0).transition().duration(duration / 2).text(this.cert.cert_short).style('opacity', 1);
+
+    const transition = this.transition(duration);
+    transition(this.axes.x.elem).call(this.axes.x.axis
+      .scale(this.axes.x.scale.domain(extent(this.data, d => d.date)).nice())
+      .tickSize(6, 0).tickFormat(date => (date.getMonth() ? timeFormat('%B') : timeFormat('%Y'))(date)));
+    transition(this.axes.y.elem).call(this.axes.y.axis
+      .scale(this.axes.y.scale.domain([0, this.data.reduce((mx, entry) => max([mx, entry.count, entry.target]), 0)]).nice())
+      .tickSize(6, 0).tickFormat(format('d')));
+    transition(this.axes.x.grid).call(this.axes.x.axis.tickSize(-this.chartHeight, 0).tickFormat(''));
+    transition(this.axes.y.grid).call(this.axes.y.axis.tickSize(-this.chartWidth, 0).tickFormat(''));
+
+    transition(this.count.area.clip).attr('d', this.target.area.above(this.data));
+    transition(this.count.area.elem).attr('d', this.count.area.below(this.data));
+    transition(this.count.line.elem).attr('d', this.count.line.fn(this.data));
+
+    transition(this.target.area.clip).attr('d', this.count.area.above(this.data));
+    transition(this.target.area.elem).attr('d', this.target.area.below(this.data));
+    transition(this.target.line.elem).attr('d', this.target.line.fn(this.data));
+
+    if (this.hovering) {
+      this.updateHighlight(duration);
     }
-  };
+  }
+
+  getClosestEntry() {
+    const date = this.axes.x.scale.invert(this.mouseX),
+      idx = dateBisector(this.data, date),
+      lo = this.data[idx - 1],
+      hi = this.data[idx];
+    if (typeof lo === 'undefined') {
+      return hi;
+    }
+    if (typeof hi === 'undefined') {
+      return lo;
+    }
+    return date - lo.date > hi.date - date ? hi : lo;
+  }
+
+  transition(duration = 50) {
+    return element => element.transition().duration(duration).ease(easing);
+  }
+
+  get hovering() {
+    return !!this.mouseX;
+  }
+
+  get margin() {
+    return { top: 20, right: 80, bottom: 40, left: 40 };
+  }
+
+  get chartWidth() {
+    return this.width - this.margin.left - this.margin.right;
+  }
+
+  get chartHeight() {
+    return this.height - this.margin.top - this.margin.bottom;
+  }
+
+  get width() {
+    return 550;
+  }
+
+  get height() {
+    return 200;
+  }
 }
 
-Promise.all([require('./certificates.sample.json'), require('./stats.sample.json')]).then(([certificates, sitesHistory]) => {
-  new Promise(function trySelectPlaceholder(resolve) {
-    if (select('svg#interactive-chart').empty()) {
-      return setTimeout(trySelectPlaceholder.bind(this, resolve), 100);
-    }
-
-    resolve(select('svg#interactive-chart'));
-  }).then(placeholder => {
-    initSvg(placeholder);
-    $scope.certificates = Object.values(certificates).sort(c => c.cert_order);
-    $scope.data = sitesHistory;
-    $scope.displayData($scope.certificates[0].cert_pk);
-    let idx = 1;
-    setInterval(() => $scope.displayData($scope.certificates[idx = (idx + 1) % $scope.certificates.length].cert_pk), 3000);
-  });
-});
-
-$scope.displayData = function (cert_pk, skipTransitions) {
-  $scope.overTime.cert = $scope.certificates.find(c => c.cert_pk === cert_pk);
-  certData = $scope.data.map(({ date, stats }) => ({ date: new Date(date), count: stats[cert_pk].current, target: stats[cert_pk].target }));
-
-  x.domain(extent(certData, d => d.date)).nice();
-  y.domain([0, certData.reduce((mx, entry) => max([mx, entry.count, entry.target]), 0)]).nice();
-
-  xAxis.scale(x);
-  yAxis.scale(y);
-
-  const duration = skipTransitions ? 0 : 1000;
-  $scope.updateHighlight(duration);
-  transition(svg.select('.x.axis'), duration).call(xAxis.tickSize(6, 0).tickFormat(date => (date.getMonth() ? timeFormat('%B') : timeFormat('%Y'))(date)));
-  transition(svg.select('.y.axis'), duration).call(yAxis.tickSize(6, 0).tickFormat(format('d')));
-
-  transition(svg.select('#clip-count path'), duration).attr('d', areaAboveCount(certData));
-  transition(svg.select('#clip-target path'), duration).attr('d', areaAboveTarget(certData));
-  transition(svg.select('.count.area'), duration).attr('d', areaBelowCount(certData));
-  transition(svg.select('.target.area'), duration).attr('d', areaBelowTarget(certData));
-
-  transition(svg.select('.target.line'), duration).attr('d', targetLine(certData));
-  transition(svg.select('.count.line'), duration).attr('d', countLine(certData));
-
-  certLegend.text($scope.overTime.cert.cert_short);
-};
+const chart = new InteractiveChart('svg#interactive-chart');
+chart.displayData(certificates[0], true);
+let idx = 1;
+setInterval(() => chart.displayData(certificates[idx = (idx + 1) % certificates.length]), 3000);
