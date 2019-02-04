64f741ba28bd36cfea3a3f5150774558b0f6b4fe
ccjmne <ccjmne@gmail.com>
2019-02-05 00:39:37 +0100

    Introduce PDF reports section

--- src/medias-section.js
+++ src/medias-section.js
@@ -1,5 +1,8 @@
+'use strict';
+
 import anime from 'animejs';
 
+// rotating certification cards
 const freezeDuration = 5000;
 anime({
   targets: '.rotating-card',
@@ -22,3 +25,16 @@ anime({
   delay: anime.stagger(1.25 * freezeDuration),
   loop: true
 });
+
+// morphing pdf report
+const px = v => `${ Math.round(v / 1.5) }px`;
+const pdf = document.querySelector('#pdf'),
+  dimensions = document.querySelector('#pdf #dimensions'),
+  orientation = document.querySelector('#pdf #orientation'),
+  duration = 500;
+
+anime.timeline({ targets: pdf, duration, easing: 'easeOutBack', loop: true, delay: 3000 })
+  .add({ width: px(297), height: px(420), changeBegin: () => anime({ targets: dimensions, opacity: [0, 1], duration, easing: 'linear', begin: () => dimensions.textContent = 'A3' }) })
+  .add({ width: px(420), height: px(297), changeBegin: () => anime({ targets: orientation, opacity: [0, 1], duration, easing: 'linear', begin: () => (pdf.classList.add('landscape'), orientation.textContent = 'paysage') }) })
+  .add({ width: px(297), height: px(210), changeBegin: () => anime({ targets: dimensions, opacity: [0, 1], duration, easing: 'linear', begin: () => dimensions.textContent = 'A4' }) })
+  .add({ width: px(210), height: px(297), changeBegin: () => anime({ targets: orientation, opacity: [0, 1], duration, easing: 'linear', begin: () => (pdf.classList.remove('landscape'), orientation.textContent = 'portrait') }) });
