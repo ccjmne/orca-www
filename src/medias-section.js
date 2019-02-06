84669e150b194db6ac744002e530118b05a1d41d
ccjmne <ccjmne@gmail.com>
2019-02-06 01:10:56 +0100

    Minor visual tweaks

--- src/medias-section.js
+++ src/medias-section.js
@@ -33,7 +33,7 @@ const pdf = document.querySelector('#pdf'),
   orientation = document.querySelector('#pdf #orientation'),
   duration = 500;
 
-anime.timeline({ targets: pdf, duration, easing: 'easeOutBack', loop: true, delay: 3000 })
+anime.timeline({ targets: pdf, duration, easing: 'easeInOutCubic', loop: true, delay: 3000 })
   .add({ width: px(297), height: px(420), changeBegin: () => anime({ targets: dimensions, opacity: [0, 1], duration, easing: 'linear', begin: () => dimensions.textContent = 'A3' }) })
   .add({ width: px(420), height: px(297), changeBegin: () => anime({ targets: orientation, opacity: [0, 1], duration, easing: 'linear', begin: () => (pdf.classList.add('landscape'), orientation.textContent = 'paysage') }) })
   .add({ width: px(297), height: px(210), changeBegin: () => anime({ targets: dimensions, opacity: [0, 1], duration, easing: 'linear', begin: () => dimensions.textContent = 'A4' }) })
