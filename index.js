54887c4a9f8ce63c3ea5fc7f06388a2f999936ab
ccjmne <ccjmne@gmail.com>
2019-02-09 00:19:21 +0100

    Pull in micromodal

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ require('./main.scss');
 require('./src/d3-fr');
 require('./src/interactive-chart/interactive-chart');
 require('./src/medias-section');
+
+import MicroModal from 'micromodal';
+MicroModal.init({ awaitCloseAnimation: true, disableScroll: false });
