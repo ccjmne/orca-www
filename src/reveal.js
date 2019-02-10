46221e148fc83cc6194f32bef17cb9350a10390e
ccjmne <ccjmne@gmail.com>
2019-02-10 02:19:06 +0100

    Refactor reveal.js leveraging OnScreen hooks

--- src/reveal.js
+++ src/reveal.js
@@ -1,18 +1,11 @@
 'use strict';
 
-import { Interval } from './utils';
+import { OnScreen } from './utils';
 
-function animate(e) {
-  e.style.visibility = 'visible';
-  return e.animate({ transform: ['translateY(20px) scale(.9)', 'translateY(0) scale(1)'], opacity: [0, 1] }, { duration: 500, easing: 'ease-out' });
-}
-
-const ival = new Interval(200);
-const stack = [];
-(observer => document.querySelectorAll('[reveal]').forEach(e => observer.observe(e)))(new IntersectionObserver((entries, obs) => entries.forEach(({ isIntersecting: reveal, target }) => {
-  if (reveal) {
-    obs.unobserve(target);
-    stack.push(target);
-    ival.do(() => (e => e ? animate(e) : ival.stop())(stack.shift()));
+new OnScreen('[reveal]', {
+  once: true,
+  enter: e => {
+    e.style.visibility = 'visible';
+    e.animate({ transform: ['translateY(20px) scale(.9)', 'translateY(0) scale(1)'], opacity: [0, 1] }, { duration: 500, easing: 'ease-out' });
   }
-}), { rootMargin: '0px', threshold: 0.15 }));
+});
