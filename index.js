90923f9abb970c244e98e4342ab26807aa57300f
ccjmne <ccjmne@gmail.com>
2019-02-14 18:06:50 +0100

    Hook up routing into main chunk

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 require('./main.scss');
-require('./src/medias-section');
 require('./src/modals/modals');
+
+import { Router } from './src/spa/routing';
 import { reveal } from './src/reveal';
 reveal(document);
+new Router('.pages-container').onLoad(reveal);
