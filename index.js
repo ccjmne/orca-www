acec594e91d957da3ee33f7cff8325c2fc62101c
ccjmne <ccjmne@gmail.com>
2019-02-15 14:57:22 +0100

    Lazy-load fonts from Google

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 require('./main.scss');
 require('./src/modals/modals');
+require('webfontloader').load({ google: { families: ['Raleway:300', 'Montserrat:300'] } });
 
 import { Router } from './src/spa/routing';
 import { reveal } from './src/reveal';
