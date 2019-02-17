1f125082b9487c05e2418781b6223a57f3ee141b
ccjmne <ccjmne@gmail.com>
2019-02-17 15:37:20 +0100

    Introduce FAQ page

---
+++ src/views/faq.js
@@ -0,0 +1,13 @@
+'use strict';
+
+export { default as html } from './faq.html';
+
+export function hook(root) {
+  [].map
+    .call(root.querySelectorAll('[data-collapse]'), e => Object.assign(e, { card: e.parentNode, collapsible: e.parentNode.querySelector('.collapsible') }))
+    .forEach(e => e.addEventListener('click', () => {
+      if (e.classList.contains('collapser') || !e.card.classList.contains('open')) {
+        e.collapsible.style.maxHeight = e.card.classList.toggle('open') ? `${ e.collapsible.scrollHeight }px` : 0;
+      }
+    }, { passive: true, useCapture: false }));
+}
