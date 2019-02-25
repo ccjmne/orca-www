2ef161e8474db9a998a54665892ded40572357a4
ccjmne <ccjmne@gmail.com>
2019-02-25 22:42:12 +0100

    Finish polishing menu component

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,15 @@
 'use strict';
 
 import { api as modals } from './modals/modals';
+import anime from 'animejs';
 
 const [menuToggle, hamburger] = [document.querySelector('.menu-toggle'), document.querySelector('.hamburger')];
 menuToggle.addEventListener('click', () => {
   if (hamburger.classList.toggle('is-active', modals.getActive() !== 'modal-menu')) {
-    return modals.open('modal-menu', { onClose: () => hamburger.classList.remove('is-active') });
+    return modals.open('modal-menu', {
+      onOpen: root => anime({ targets: root.querySelectorAll('.menu-items > a'), translateX: [-50, 0], opacity: [0, 1], easing: 'easeOutBack', delay: anime.stagger(100, { start: 200 }), duration: 500 }),
+      onClose: () => hamburger.classList.remove('is-active')
+    });
   }
 
   modals.close();
--- src/modals/modals.js
+++ src/modals/modals.js
@@ -24,7 +24,7 @@ export const api = {
           keyframes: [{ easing: 'easeInCubic', translateZ: '-50vw', rotateY: '25deg' }, { easing: 'easeOutBack', translateZ: '-100vw', rotateY: 0 }]
         }).add({ targets: overlay, opacity: .6, easing: 'easeInOutExpo' }, 0);
         if (typeof onOpen === 'function') {
-          onOpen();
+          onOpen(modals.querySelector(`#${ modalId }`));
         }
       },
       onClose: () => {
@@ -33,7 +33,7 @@ export const api = {
           keyframes: [{ easing: 'easeInCubic', translateZ: '-50vw', rotateY: '20deg' }, { easing: 'easeOutBack', translateZ: 0, rotateY: 0 }]
         }).add({ targets: overlay, opacity: 0, easing: 'easeInOutExpo' }, 0).finished.then(() => api._clearActive());
         if (typeof onClose === 'function') {
-          onClose();
+          onClose(modals.querySelector(`#${ modalId }`));
         }
       }
     });
