41f3c842b4eab5d547338a1617aaf392d9e781ed
ccjmne <ccjmne@gmail.com>
2019-02-25 03:34:30 +0100

    WIP: implement menu as a modal

--- src/modals/modals.js
+++ src/modals/modals.js
@@ -2,11 +2,15 @@
 
 import MicroModal from 'micromodal';
 import anime from 'animejs';
-import { menu } from '../menu';
 
-const [wrapper, overlay, modals] = [document.querySelector('.main-wrapper'), document.querySelector('.main > .overlay'), document.querySelector('.modals-container')];
+export const api = {
+  setActive(current) { this._current = current; },
+  clearActive() { this._current = null; },
+  close() { if (this._current) { MicroModal.close(); } }
+};
+
+const [wrapper, overlay, modals] = [document.querySelector('.outer-wrapper'), document.querySelector('.main > .overlay'), document.querySelector('.modals-container')];
 document.querySelectorAll('[data-modal-trigger]').forEach(trigger => trigger.addEventListener('click', async function () {
-  menu.hide();
   const modalId = this.getAttribute('data-modal-trigger');
   if (!modals.querySelector(`#${ modalId }`)) {
     const { 'default': modal } = await import( /* webpackChunkName: 'modals/[request]' */ `./${ modalId }.html`);
@@ -15,13 +19,13 @@ document.querySelectorAll('[data-modal-trigger]').forEach(trigger => trigger.add
 
   MicroModal.show(modalId, {
     awaitCloseAnimation: true,
-    onShow: () => anime.timeline({
+    onShow: () => (api.setActive(modalId), anime.timeline({
       targets: wrapper,
-      keyframes: [{ easing: 'easeInExpo', translateZ: -50, rotateY: '5deg' }, { easing: 'easeOutBack', translateZ: -200, rotateY: 0 }]
-    }).add({ targets: overlay, opacity: .6, easing: 'easeInOutExpo' }, 0),
+      keyframes: [{ easing: 'easeInCubic', translateZ: '-50vw', rotateY: '20deg' }, { easing: 'easeOutBack', translateZ: '-100vw', rotateY: 0 }]
+    }).add({ targets: overlay, opacity: .6, easing: 'easeInOutExpo' }, 0)),
     onClose: () => anime.timeline({
       targets: wrapper,
-      keyframes: [{ easing: 'easeInExpo', translateZ: -50, rotateY: '5deg' }, { easing: 'easeOutBack', translateZ: 0, rotateY: 0 }]
-    }).add({ targets: overlay, opacity: 0, easing: 'easeInOutExpo' }, 0).finished.then(menu.show)
+      keyframes: [{ easing: 'easeInCubic', translateZ: '-50vw', rotateY: '-20deg' }, { easing: 'easeOutBack', translateZ: 0, rotateY: 0 }]
+    }).add({ targets: overlay, opacity: 0, easing: 'easeInOutExpo' }, 0).finished.then(() => api.clearActive())
   });
 }, { passive: true, useCapture: false }));
--- src/spa/routing.js
+++ src/spa/routing.js
@@ -1,11 +1,13 @@
 'use strict';
 
 import './pushstate-anchor';
-import { menu } from '../menu';
+import { api as modals } from '../modals/modals';
+import anime from 'animejs';
 
 export class Router {
   constructor(selector) {
     this.root = document.querySelector(selector);
+    this.wrapper = document.querySelector('.main-wrapper');
     window.addEventListener('popstate', () => this._loadPage(), { passive: true, useCapture: false });
     this._loadPage();
   }
@@ -18,13 +20,18 @@ export class Router {
   async _loadPage() {
     const view = window.location.pathname.substr(1) || 'home';
     const { html, hook: pageHook } = await import( /* webpackChunkName: 'partials/[request]' */ `../views/${ view }/${ view }.js`);
-    this.root.innerHTML = html;
-    menu.close();
-    if (typeof pageHook === 'function') {
-      pageHook(this.root);
-    }
-    if (typeof this.hook === 'function') {
-      this.hook(this.root);
-    }
+    // TODO: scroll to top
+    anime.timeline({ targets: this.wrapper })
+      .add({ rotateY: [0, '-90deg'], duration: 500, easing: 'easeInBack', changeComplete: () => this.root.innerHTML = html })
+      .add({ rotateY: ['90deg', 0], duration: 300, easing: 'easeOutCubic' })
+      .finished.then(() => {
+        modals.close();
+        if (typeof pageHook === 'function') {
+          pageHook(this.root);
+        }
+        if (typeof this.hook === 'function') {
+          this.hook(this.root);
+        }
+      });
   }
 }
