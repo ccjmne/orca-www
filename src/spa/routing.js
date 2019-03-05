1f25e143fa1c1b1723cf2aca822d6c0c9cb5e8c4
ccjmne <ccjmne@gmail.com>
2019-03-05 15:18:06 +0100

    Bundle dynamic pages under pages/ (was views/)

--- src/spa/routing.js
+++ src/spa/routing.js
@@ -19,7 +19,7 @@ export class Router {
 
   async _loadPage(init = false) {
     const view = window.location.pathname.substr(1) || 'home';
-    const { html, hook: pageHook, onLoadOpts } = await import( /* webpackChunkName: 'partials/[request]' */ `../views/${ view }/${ view }.js`);
+    const { html, hook: pageHook, onLoadOpts } = await import( /* webpackChunkName: 'pages/[request]' */ `../views/${ view }/${ view }.js`);
     Promise.resolve(init ? this.root.innerHTML = html : anime.timeline({ targets: this.wrapper })
         .add({ rotateY: [0, '90deg'], duration: 500, easing: 'easeInBack', changeComplete: () => (this.root.innerHTML = html, this.root.scrollIntoView(true)) })
         .add({ rotateY: ['-90deg', 0], duration: 300, easing: 'easeOutCubic' })
